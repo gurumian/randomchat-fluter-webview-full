fba0e9cd6507e8a36217c793d754a68a5e370c7d
Bryce Ghim <gurumlab@gmail.com>
2024-12-03 18:49:08 +0900

    init draft

---
+++ android/app/src/main/kotlin/com/example/randomchat_flutter_webview_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.randomchat_flutter_webview_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  InAppWebViewController? _webViewController;
+  double progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // _requestPermissions();
+  }
+
+  Future<void> _requestPermissions() async {
+    await Permission.camera.request();
+    await Permission.microphone.request();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('InAppWebView Example'),
+      ),
+      body: Column(
+        children: [
+          progress < 1.0
+              ? LinearProgressIndicator(value: progress)
+              : Container(),
+          Expanded(
+            child: InAppWebView(
+              initialUrlRequest:
+                  URLRequest(url: WebUri("https://randomchat.papergirl.site")),
+              initialSettings: InAppWebViewSettings(
+                mediaPlaybackRequiresUserGesture: false,
+                allowsInlineMediaPlayback: true,
+                javaScriptEnabled: true,
+                // useShouldOverrideUrlLoading: true,
+              ),
+              onWebViewCreated: (InAppWebViewController controller) {
+                _webViewController = controller;
+              },
+              onLoadStart: (controller, url) {
+                setState(() {
+                  progress = 0;
+                });
+              },
+              onProgressChanged: (controller, progress) {
+                setState(() {
+                  this.progress = progress / 100;
+                });
+              },
+              onPermissionRequest: (controller, request) async {
+                return PermissionResponse(
+                    resources: request.resources,
+                    action: PermissionResponseAction.GRANT);
+              },
+              onReceivedError: (controller, request, error) {
+                print('WebView error: ${error.description}');
+                // You can show an error message to the user here
+              },
+              onConsoleMessage: (controller, consoleMessage) {
+                print('Console message: ${consoleMessage.message}');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
